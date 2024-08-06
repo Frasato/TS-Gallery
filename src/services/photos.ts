4ffe21a9387aed821c048a838017d51b07901739
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-06 09:17:21 -0300

    Create a list of all images in folder on firebase storage

---
+++ src/services/photos.ts
@@ -0,0 +1,22 @@
+import { Photo } from "../types/Photo";
+import { storage } from "../libs/firebase";
+import { ref, listAll, getDownloadURL } from 'firebase/storage';
+
+export const getAll = async () =>{
+    const list: Photo[] = [];
+
+    const imagesFolder = ref(storage, "Images");
+    const photoList = await listAll(imagesFolder);
+    
+    for(const i in photoList.items){
+        
+        const photoUrl = await getDownloadURL(photoList.items[i]);
+
+        list.push({
+            name: photoList.items[i].name,
+            url: photoUrl,
+        })
+    }
+
+    return list;
+}
