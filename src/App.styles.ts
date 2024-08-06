5aa5ab07cd2d847e750fbf42ed15331890326294
Gabriel Frasato <gabriel.frasato@hotmail.com>
2024-08-06 08:50:34 -0300

    Create styled components and create the page on app

---
+++ src/App.styles.ts
@@ -0,0 +1,20 @@
+import { styled } from "styled-components";
+
+export const Container = styled.div`
+    background-color: #27282F;
+    color: #FFF;
+    min-height: 100vh;
+`;
+
+export const Area = styled.div`
+    margin: auto;
+    max-width: 980px;
+    padding: 30px 0;
+`;
+
+export const Header = styled.h1`
+    text-align: center;
+    margin-bottom: 30px;
+    font-weight: bold;
+    font-size: 40px;
+`;
--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
-function App() {
+import * as C from './App.styles.ts';
+
+const App = () => {
   return (
-    <>
-      
-    </>
+    <C.Container>
+      <C.Area>
+        <C.Header>Galeria de fotos</C.Header>
+        {/* Area de upload */}
+
+        {/* Lista de fotos */}
+      </C.Area>
+    </C.Container>
   )
 }
 
